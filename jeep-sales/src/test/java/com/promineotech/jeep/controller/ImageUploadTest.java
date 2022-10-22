8dfc12e54a4ce9c37312c28702467028bf28bc44
SOHAIL KHAN <68357473+sohailjaan2002@users.noreply.github.com>
2022-10-22 19:05:54 -0400

    Add files via upload

---
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/ImageUploadTest.java
@@ -0,0 +1,58 @@
+package com.promineotech.jeep.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ImageUploadTest {
+  
+  private static final String JEEP_IMAGE = "JeepWrangler.jpg";
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void testThatTheServerCorrectlyRecievesAnImageAndReturnsAnOKResponse()
+    throws Exception{
+    Resource image = new ClassPathResource(JEEP_IMAGE);
+    
+    if(!image.exists()){
+      fail("Could not find resource %s", JEEP_IMAGE);
+    }
+    
+    try(InputStream inputStream = image.getInputStream()){
+      MockMultipartFile file = new MockMultipartFile("image", JEEP_IMAGE,
+          MediaType.TEXT_PLAIN_VALUE, inputStream);
+      
+      //@formatter:off
+      MvcResult result = mockMvc
+          .perform(MockMvcRequestBuilders
+              .multipart("/jeeps/1/image")
+              .file(file))
+          .andDo(print())
+          .andExpect(status().isOk())
+          .andReturn();
+          //@formatter:on
+      
+      String content = result.getResponse().getContentAsString();
+      assertThat(content).isNotEmpty();
+    }
+  }
+  
+}
